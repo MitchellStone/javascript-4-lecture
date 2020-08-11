e3fbeb4767e7eb57b10d15269ff43d53cffacb21
mitchell stone <themitch.stone@gmail.com>
2020-08-11 10:39:53 -0600

    added JS

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ console.log("I am working")
 
 
 let headerText = document.querySelector(".title")
-
+headerText.innerText = "Potato Juice"
+headerText.Style = "Color: purple; , text-decoration: underline;"
 console.log(headerText)
